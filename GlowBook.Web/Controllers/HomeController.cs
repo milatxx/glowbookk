b6092bdad46c2736b34ab39deb6485c66f344247
milatxx <146560367+milatxx@users.noreply.github.com>
2025-11-22 18:43:16 +0100

    Set up base MVC layout, home views and initial EF Core migration

--- GlowBook.Web/Controllers/HomeController.cs
+++ GlowBook.Web/Controllers/HomeController.cs
@@ -1,32 +1,17 @@
-using GlowBook.Web.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using System.Diagnostics;
 
-namespace GlowBook.Web.Controllers
+namespace GlowBook.Web.Controllers;
+
+[Authorize]
+public class HomeController : Controller
 {
-    public class HomeController : Controller
-    {
-        private readonly ILogger<HomeController> _logger;
+    public IActionResult Index() => View();
 
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
+    [AllowAnonymous]
+    public IActionResult Privacy() => View();
 
-        public IActionResult Index()
-        {
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
-    }
+    [AllowAnonymous]
+    public IActionResult Error() => View();
 }
+    
